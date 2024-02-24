a239fc0a31fb4207b5540e883653acb270e9c5d4
Iner <inercollado52@gmail.com>
2024-02-24 14:03:07 +0800

    fibonacci sequence (getting nth based on users input

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ let nextNum;
 
 let array = [];
 
-
+let num = prompt("Enter a number: ");
+num = parseInt(num)
 
 function fibonacci() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < num - 2; i++) {
         nextNum = prevNum + currentNum;
-
         array.push(nextNum);
-
         prevNum = currentNum;
         currentNum = nextNum;
-
-        if (i == 9) {
-            prevNum = 0;
-            currentNum = 1;
-        }
     }
+    prevNum = 0;
+    currentNum = 1;
+
     console.log(`${prevNum} ${currentNum} ${array.join(' ')}`);
 }
-   
+//    2 3 1 2 3
 fibonacci();
     
 
